0e834948d748ecf90699fa673a6a2b4627b5979f
m222zu <147124424+m222zu@users.noreply.github.com>
2025-10-18 18:41:30 +0900

    feat: user entity 생성

--- src/main/java/com/example/timefit/domain/user/entity/User.java
+++ src/main/java/com/example/timefit/domain/user/entity/User.java
@@ -1,5 +1,48 @@
 package com.example.timefit.domain.user.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class User {
-  
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true)
+  private String socialId;
+
+  @Column(nullable = false)
+  private String provider;
+
+  @Column(nullable = false)
+  private String nickname;
+
+  @Column
+  private String profileImage;
+
+  @Column(updatable = false)
+  private LocalDateTime createdAt;
+
+  @Column
+  private LocalDateTime updatedAt;
+
+  @PrePersist
+  protected void onCreate() {
+    this.createdAt = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  protected void onUpdate() {
+    this.updatedAt = LocalDateTime.now();
+  }
 }
