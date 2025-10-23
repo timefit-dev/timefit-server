e3071ed7bcf0e4aeb989617bca914bc4fbd87cac
jangjuntae <juntae0629@naver.com>
2025-10-23 20:32:05 +0900

    FEAT:Room ENTITY, RoomDate ENTITY 설정

--- src/main/java/com/example/timefit/domain/room/entity/Room.java
+++ src/main/java/com/example/timefit/domain/room/entity/Room.java
@@ -3,7 +3,6 @@ package com.example.timefit.domain.room.entity;
 import com.example.timefit.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,7 +13,6 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Room {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
@@ -28,7 +26,13 @@ public class Room {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "owner_id", nullable = false)
-    private User ownerId;
+    private User owner;
+
+    @Column(name = "start_time", length = 20, nullable = false)
+    private String startTime;
+
+    @Column(name = "end_time", length = 20, nullable = false)
+    private String endTime;
 
     @Column(name = "created_at", updatable = false, nullable = false)
     private LocalDateTime createdAt;
@@ -39,27 +43,16 @@ public class Room {
     @Column(name = "expires_at", nullable = false)
     private LocalDateTime expiresAt;
 
-    @Builder
     public Room(String title, String inviteCode, User owner) {
         this.title = title;
         this.inviteCode = inviteCode;
-        this.ownerId = owner;
+        this.owner = owner;
     }
 
-    // JPA가 이 엔티티를 DB에 'INSERT' 쿼리를 날리기 직전에 호출됩니다.
-    @PrePersist
     public void onPrePersist() {
         LocalDateTime now = LocalDateTime.now();
         this.createdAt = now;
         this.updatedAt = now;
-        this.expiresAt = now.plusDays(2); // 생성기한 + 2일
-    }
-
-     // JPA가 이 엔티티를 'UPDATE' 쿼리를 날리기 직전에 호출됩니다.
-    @PreUpdate
-    public void onPreUpdate() {
-        LocalDateTime now = LocalDateTime.now();
-        this.updatedAt = now;
-        this.expiresAt = now.plusDays(2); // 수정기한 + 2일
+        this.expiresAt = now.plusDays(2);
     }
 }
--- src/main/java/com/example/timefit/domain/room/entity/RoomDate.java
+++ src/main/java/com/example/timefit/domain/room/entity/RoomDate.java
@@ -2,22 +2,15 @@ package com.example.timefit.domain.room.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "room_dates", uniqueConstraints = {
-        @UniqueConstraint(
-                name = "DATE_ROOM_UNIQUE", // DB에 표시될 제약조건 이름
-                columnNames = {"room_id", "date"} // 이 두 컬럼의 조합은 유일해야 함
-        )
-})
+@Table(name = "room_dates")
 public class RoomDate {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,7 +24,6 @@ public class RoomDate {
     @JoinColumn(name = "room_id", nullable = false)
     private Room room;
 
-    @Builder
     public RoomDate(LocalDate date, Room room) {
         this.date = date;
         this.room = room;
