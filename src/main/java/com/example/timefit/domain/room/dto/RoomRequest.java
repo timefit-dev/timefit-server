1ad7f7696e4449e0c8c3a87c8f8e3a60fb7a36e3
jangjuntae <juntae0629@naver.com>
2025-10-26 18:35:21 +0900

    FEAT: RoomDTO 개발

--- src/main/java/com/example/timefit/domain/room/dto/RoomRequest.java
+++ src/main/java/com/example/timefit/domain/room/dto/RoomRequest.java
@@ -1,5 +1,24 @@
 package com.example.timefit.domain.room.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
 public class RoomRequest {
-  
+    @Getter
+    @NoArgsConstructor
+    public static class CreateDTO{
+        private String title;
+        private List<LocalDate> dates;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+        private LocalTime startTime;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+        private LocalTime endTime;
+    }
 }
--- src/main/java/com/example/timefit/domain/room/dto/RoomResponse.java
+++ src/main/java/com/example/timefit/domain/room/dto/RoomResponse.java
@@ -1,5 +1,17 @@
 package com.example.timefit.domain.room.dto;
 
+import com.example.timefit.domain.room.entity.Room;
+import lombok.Getter;
+
 public class RoomResponse {
-  
+    @Getter
+    public static class CreateDTO {
+        private final Long roomId;
+        private final String inviteCode;
+
+        public CreateDTO(Room room) {
+            this.roomId = room.getId();
+            this.inviteCode = room.getInviteCode();
+        }
+    }
 }
