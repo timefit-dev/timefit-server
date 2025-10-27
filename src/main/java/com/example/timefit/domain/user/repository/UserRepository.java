693b84249e4a5540c052b0cc445f02366e95138e
m222zu <147124424+m222zu@users.noreply.github.com>
2025-10-27 23:53:31 +0900

    feat: 유저 조회 메서드 추가

--- src/main/java/com/example/timefit/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/timefit/domain/user/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.example.timefit.domain.user.repository;
 
-public class UserRepository {
-  
+import com.example.timefit.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findBySocialId(String socialId);
 }
