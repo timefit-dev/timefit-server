7da87682f9d8e2a06f6194792f3c2ef68d85f558
m222zu <147124424+m222zu@users.noreply.github.com>
2025-10-27 23:56:49 +0900

    feat: 유저 조회 로직 및 dto 변환

--- src/main/java/com/example/timefit/domain/user/dto/UserResponse.java
+++ src/main/java/com/example/timefit/domain/user/dto/UserResponse.java
@@ -1,5 +1,20 @@
 package com.example.timefit.domain.user.dto;
 
+import com.example.timefit.domain.user.entity.User;
+import lombok.Getter;
+
+@Getter
 public class UserResponse {
-  
+  private final Long id;
+  private final String socialId;
+  private final String nickname;
+  private final String profileImage;
+
+  public UserResponse(User user) {
+    this.id = user.getId();
+    this.socialId = user.getSocialId();
+    this.nickname = user.getNickname();
+    this.profileImage = user.getProfileImage();
+  }
+
 }
--- src/main/java/com/example/timefit/domain/user/service/UserService.java
+++ src/main/java/com/example/timefit/domain/user/service/UserService.java
@@ -1,5 +1,24 @@
 package com.example.timefit.domain.user.service;
 
+import com.example.timefit.domain.user.dto.UserResponse;
+import com.example.timefit.domain.user.entity.User;
+import com.example.timefit.domain.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class UserService {
+
+  private final UserRepository userRepository;
+
+  public UserService(UserRepository userRepository) {
+      this.userRepository = userRepository;
+  }
+
+  public UserResponse getUserBySocialId(String socialId) {
+    User user = userRepository.findBySocialId(socialId)
+            .orElseThrow(() -> new IllegalArgumentException("해당 유저를 찾을 수 없습니다. socialId =" + socialId));
+    return new UserResponse(user);
+  }
   
 }
