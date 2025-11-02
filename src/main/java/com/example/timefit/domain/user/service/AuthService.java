1c1089ed37db14a7b3c1c16d12773c961eaa9591
m222zu <147124424+m222zu@users.noreply.github.com>
2025-11-02 17:07:19 +0900

    feat: AuthService 기본 틀 작성

--- src/main/java/com/example/timefit/domain/user/service/AuthService.java
+++ src/main/java/com/example/timefit/domain/user/service/AuthService.java
@@ -1,5 +1,17 @@
 package com.example.timefit.domain.user.service;
 
+import org.springframework.stereotype.Service;
+
+import com.example.timefit.domain.user.dto.AuthRequest;
+import com.example.timefit.domain.user.dto.AuthResponse;
+
+@Service
 public class AuthService {
   
+  public AuthResponse socialLogin(AuthRequest request) {
+    //카카오인가코드 받아오는 로직 추가
+    System.out.println("provider: " + request.provider());
+    System.out.println("authorizationCode: " + request.authorizationCode());
+    return null;
+  }
 }
