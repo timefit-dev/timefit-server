527654c9ddf9a76b5d16f05ecf5e4dde11763b86
m222zu <147124424+m222zu@users.noreply.github.com>
2025-11-02 17:04:55 +0900

    feat: 로그인 엔드포인트 추가

--- src/main/java/com/example/timefit/domain/user/controller/AuthController.java
+++ src/main/java/com/example/timefit/domain/user/controller/AuthController.java
@@ -1,5 +1,20 @@
 package com.example.timefit.domain.user.controller;
 
+import com.example.timefit.domain.user.dto.AuthRequest;
+import com.example.timefit.domain.user.dto.AuthResponse;
+import com.example.timefit.domain.user.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
   
+  private final AuthService authService;
+
+  @PostMapping("/login")
+  public AuthResponse socialLogin(@RequestBody AuthRequest request) {
+    return authService.socialLogin(request);
+  }
 }
