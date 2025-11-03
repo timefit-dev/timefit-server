c2071682b3171f4a382bf9932bb366912a3ec587
m222zu <147124424+m222zu@users.noreply.github.com>
2025-11-03 21:54:29 +0900

    refactor: AuthRequest/AuthResponse 삭제 및 LoginRequest/LoginResponse로 교체

--- src/main/java/com/example/timefit/domain/user/dto/AuthRequest.java
+++
@@ -1,6 +0,0 @@
-package com.example.timefit.domain.user.dto;
-
-public record AuthRequest(
-  String provider,
-  String authorizationCode
-) {}
--- src/main/java/com/example/timefit/domain/user/dto/AuthResponse.java
+++
@@ -1,7 +0,0 @@
-package com.example.timefit.domain.user.dto;
-
-public record AuthResponse(
-    String accessToken,
-    String refreshToken,
-    UserResponse user
-) {}
