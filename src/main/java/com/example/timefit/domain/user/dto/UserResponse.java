fe0463a8e6271d4e8396b973e9efd9c38e9e518f
m222zu <147124424+m222zu@users.noreply.github.com>
2025-11-12 21:00:42 +0900

    feat: UserResponse에 provider 필드 추가

--- src/main/java/com/example/timefit/domain/user/dto/UserResponse.java
+++ src/main/java/com/example/timefit/domain/user/dto/UserResponse.java
@@ -6,9 +6,10 @@ public record UserResponse (
   Long id,
   String socialId,
   String nickname,
-  String profileImage
+  String profileImage,
+  String provider
 ) {
   public UserResponse(User user) {
-    this(user.getId(), user.getSocialId(), user.getNickname(), user.getProfileImage());
+    this(user.getId(), user.getSocialId(), user.getNickname(), user.getProfileImage(), user.getProvider());
   }
 }
