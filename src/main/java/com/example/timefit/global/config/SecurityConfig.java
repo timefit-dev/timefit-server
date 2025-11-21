33ffeb93fc14c4a9998c07ece9d98e17943f8c89
m222zu <147124424+m222zu@users.noreply.github.com>
2025-11-22 00:22:50 +0900

    feat: jwt 인증구조로 변경

--- src/main/java/com/example/timefit/global/config/SecurityConfig.java
+++ src/main/java/com/example/timefit/global/config/SecurityConfig.java
@@ -1,33 +1,46 @@
 package com.example.timefit.global.config;
 
 import com.example.timefit.domain.user.service.AuthService;
+import com.example.timefit.domain.user.service.OAuth2SuccessHandler;
+import com.example.timefit.global.jwt.JwtAuthenticationFilter;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final AuthService authService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
-            .headers(headers -> headers.frameOptions(frame -> frame.disable()))
-            .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+            .csrf(csrf -> csrf.disable())
+            .formLogin(form -> form.disable())
+            .httpBasic(basic -> basic.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/login", "/css/**", "/js/**", "/h2-console/**").permitAll()
-                .anyRequest().authenticated()
+                    .requestMatchers("/", "/login", "/oauth2/**").permitAll()
+                    .anyRequest().authenticated()
             )
-            
             .oauth2Login(oauth -> oauth
                 .userInfoEndpoint(user -> user.userService(authService))
-                .defaultSuccessUrl("/", true)
-            );
+                .successHandler(oAuth2SuccessHandler)
+            )
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
