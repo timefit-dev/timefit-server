354297b8a897482e649de5828c9039d60eb2ef4f
m222zu <147124424+m222zu@users.noreply.github.com>
2025-12-04 21:44:11 +0900

    refactor: 불필요한 user필드 삭제

--- src/main/java/com/example/timefit/domain/user/dto/UserResponse.java
+++ src/main/java/com/example/timefit/domain/user/dto/UserResponse.java
@@ -5,11 +5,9 @@ import com.example.timefit.domain.user.entity.User;
 public record UserResponse (
   Long id,
   String socialId,
-  String nickname,
-  String profileImage,
   String provider
 ) {
   public UserResponse(User user) {
-    this(user.getId(), user.getSocialId(), user.getNickname(), user.getProfileImage(), user.getProvider());
+    this(user.getId(), user.getSocialId(), user.getProvider());
   }
 }
